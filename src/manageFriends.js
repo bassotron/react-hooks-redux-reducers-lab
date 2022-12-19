5baade295fbd61ac6514bff110de64a1b9416470
Will Lovell <91235913+bassotron@users.noreply.github.com>
2022-12-19 12:49:31 -0700

    commit tree 0aa9657c3ec04c2f327a9a9bba70505683d45f33

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,16 @@
+const state = { friends: [] }
+const action = { type: "friends/add", 
+payload: { name: "Will", hometown: "Denver", id: 1 }}
+
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+    return { friends: [...state.friends, action.payload] }
+    case "friends/remove":
+    return {
+      friends: state.friends.filter((friend) => friend.id !== action.payload) 
+    };
+    default:
+      return state;
+  }
 }
